2447c981d6bdd33167a98ba0faab6b9c8422a9db
Carlos Delgado <carlos.dz16180@gmail.com>
2025-10-30 12:58:45 +0100

    Pull para actualizar lo que hemos hecho en lo de cloud

--- 04-cloud/app.js
+++ 04-cloud/app.js
@@ -1,13 +1,23 @@
 import express from 'express';
+import fs from 'fs';
+
+import ENV from './utils/envLoader.js';
+import fileRouter from './routers/file.router.js';
 
 const app = express();
 
 app.use(express.json());
 
+if (!fs.existsSync(ENV.CLOUD_STORAGE_PATH)) {
+    fs.mkdirSync(ENV.CLOUD_STORAGE_PATH, { recursive: true }); // mkdir -p
+}
+
 app.get('/', (req, res) => {
   res.send('Hello, Cloud!');
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.use('/cloud', fileRouter)
+
+app.listen(ENV.PORT, () => {
+    console.log('Server is running on port: ' + ENV.PORT);
 });
---
+++ 04-cloud/utils/multer.js
@@ -0,0 +1,26 @@
+import multer from 'multer';
+import path from 'path';
+import ENV from './envLoader.js';
+
+//
+
+const storage = multer.diskStorage({
+
+    // Define el directorio donde se guarden los ficheros
+    // cb = function cb(error, dato);
+    destination: ENV.CLOUD_STORAGE_PATH,
+    
+    filename: (_req, file, cb) => {
+
+        // basename: '../../etc/passwd.conf' => 'passwd.conf'
+        const safeName = path.basename(file.originalname)
+        cb(null, safeName)
+    }
+});
+
+export const upload = multer({ 
+    storage: storage,
+    limits: {
+        fileSize: ENV.MAX_FILE_SIZE_MB * 1024 * 1024, // MB
+    }
+});
